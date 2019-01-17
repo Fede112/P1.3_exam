6919dc1fc22f2a4e58f7b37d86d65bbf251a70c8
jesusEV <jesus.espinoza.val@gmail.com>
2019-01-17 17:25:47 +0100

    writeFile implemented

--- python/exam.py
+++ python/exam.py
@@ -8,9 +8,9 @@ class PostcardList():
 	def __init__(self):
 		self._file = None
 		self._postcards = [] 
-		self._date = {}
-		self._from = {}
-		self._to = {}
+		self._date = {"2002":1, "2003":2, "2004":4}
+		self._from = {"A":1, "B":2, "C":4}
+		self._to = {"D":1, "E":2, "F":4}
 
 	def readFile(self, file_path):
 		self._file = file_path
@@ -20,6 +20,19 @@ class PostcardList():
 
 	def getNumberOfPostcards(self):
 		return len(self._postcards)
+
+		
+	def writeFile(self, file_path):
+		'''
+		def writeFile(self):
+			with open(self._file, 'w') as file:
+		'''
+		with open(file_path, 'w') as file:
+			iterable = zip(self._date.keys(), self._from.keys(), self._to.keys())
+			for date, fromm, to in iterable:
+				line = "date:{d}; from:{f}; to:{t};\n".format(\
+							d=date, f=fromm, t=to)
+				file.write(line)
 
 
 
@@ -35,6 +48,7 @@ if __name__ == '__main__':
 
 	numberOfPostcards = postL.getNumberOfPostcards()
 
+	postL.writeFile("./test.txt")
 	print(postL._file)
 	print(postL._postcards)
 	print(numberOfPostcards)
