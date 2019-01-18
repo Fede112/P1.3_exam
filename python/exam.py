c1f265f3f763bbf895f0c97311e63589aa62d97c
jesusEV <jesus.espinoza.val@gmail.com>
2019-01-18 11:28:02 +0100

    almost complete

--- python/exam.py
+++ python/exam.py
@@ -11,26 +11,61 @@ class PostcardList():
 		self._date = {}
 		self._from = {}
 		self._to = {}
-		# self._date = {"2002":1, "2003":2, "2004":4}
-		# self._from = {"A":1, "B":2, "C":4}
-		# self._to = {"D":1, "E":2, "F":4}
 
-	def readFile(self, file_path):
-		self._file = file_path
+	def readFile(self, from_file_path):
+	# def readFile(self):
+		# This function initialize the PostcardList with _file = from_file_path
+		# Only for initialization 
+		self._file = from_file_path
 		self._postcards = []
-		with open(file_path) as file: 
+		with open(from_file_path) as file: 
 			for pc in file:  
 				self._postcards.append(pc.rstrip())
-		# self._parsePostcards()
+		self.parsePostcards()
 
-	def getNumberOfPostcards(self):
-		return len(self._postcards)
 
-	def _parsePostcards(self):
+	def updateLists(self, from_file_path):
+		# Updates object to include new Postcards from another file 
+		pc_list = []
+		with open(from_file_path) as file: 
+			for pc in file:  
+				self._postcards.append(pc.rstrip())
+				pc_list.append(pc)
+		self.parsePostcards()
+		print(" Print pc_lists: ")
+		print(pc_list)
+		self.updateFile(pc_list)
+
+	def updateFile(self, pc_list):
+		'''
+		Update _file associated with PostcardLists.
+		Meant to be a private function called by updateLists(). Do not run by it self!!
+		'''
+		with open(self._file, 'a') as file:
+			for pc in pc_list:
+				file.write(pc)
+
+			
+	def writeFile(self, to_file_path):
+		'''
+     		def writeFile(self):
+			with open(self._file, 'w') as file:
+		'''
+		with open(to_file_path, 'w') as file:
+			iterable = zip(self._date.keys(), self._from.keys(), self._to.keys())
+			for date, fromm, to in iterable:
+				line = "date:{d}; from:{f}; to:{t};\n".format(\
+							d=date, f=fromm, t=to)
+				file.write(line)
+
+
+	def parsePostcards(self):
 		prev_len = len(self._date) 
-
+		print(self._postcards)
+		# print(self._postcards[prev_len:self.getNumberOfPostcards()])
 		for record, pc in enumerate(self._postcards[prev_len:self.getNumberOfPostcards()],1):
 			record += prev_len
+			# print(pc)
 			date_key =  pc.split(';')[0].split(':')[1]
 			from_key = pc.split(';')[1].split(':')[1]
 			to_key = pc.split(';')[2].split(':')[1]
@@ -47,18 +82,6 @@ class PostcardList():
 			self._date[ date_key ].append(record)  
 			self._from[ from_key ].append(record)  
 			self._to[ to_key  ].append(record)   
-			
-	def writeFile(self, file_path):
-		'''
-		def writeFile(self):
-			with open(self._file, 'w') as file:
-		'''
-		with open(file_path, 'w') as file:
-			iterable = zip(self._date.keys(), self._from.keys(), self._to.keys())
-			for date, fromm, to in iterable:
-				line = "date:{d}; from:{f}; to:{t};\n".format(\
-							d=date, f=fromm, t=to)
-				file.write(line)
 
 	def getPostcardsBySender(self, sender):
 		pcs = []
@@ -81,25 +104,37 @@ class PostcardList():
 		pcInRange = [ self._postcards[i-1] for i in datesInRange ]
 		print(pcInRange)
 
+	def getNumberOfPostcards(self):
+		return len(self._postcards)
+
 if __name__ == '__main__':
 
 	curret_directory = os.getcwd()
 
-	file_name = 'exam_postcard_list4.txt'
+	file_name = 'test0.txt'
+	other_file_name = 'test.txt'
+
 	file_path = curret_directory + "/" + file_name
+	other_file_path = curret_directory + "/" + other_file_name
 
 	postL = PostcardList()
 	postL.readFile(file_path)
+	print("antes \n", postL._from)
+	postL.updateLists(other_file_path)
+	print("despues \n", postL._from)
+	# postL.writeFile("./test.txt")
+
 
 	numberOfPostcards = postL.getNumberOfPostcards()
-	postL._parsePostcards()
+	# postL.parsePostcards()
 	# print(postL._postcards)
-	postL.writeFile("./test.txt")
+
 	# print(postL._file)
-	# print(postL._from)
 	# print(postL.getPostcardsBySender("Dewey"))
 	# print(postL.getPostcardsByReceiver("Dewey"))
 	postL.getPostcardsByDateRange(datetime.datetime.strptime("2010-01-01", "%Y-%m-%d"),datetime.datetime.strptime("2010-10-08", "%Y-%m-%d") )
 	# print(postL._file)
 	# print(postL._postcards)
 	print(numberOfPostcards)
+
+	# postL.updateFile(other_file_path)
